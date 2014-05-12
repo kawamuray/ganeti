8a4cb0701e89c96037a05b49926e1901b9bcea38
Yuto KAWAMURA(kawamuray) <kawamuray.dadada@gmail.com>
2014-05-12 13:53:41 +0900

    Added regex-pcre-builtin which is conflicting with regex-pcre to -hide-package flag

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 make clean
 ./autogen.sh
-./configure --enable-symlinks --with-haskell-flags='-hide-package=monads-tf'
+./configure --enable-symlinks --with-haskell-flags="\
+-hide-package=monads-tf \
+-hide-package=regex-pcre-builtin \
+"
 make -j2
