a4a7c984177a64da6b690a4503995972c83c2429
Yuto KAWAMURA(kawamuray) <kawamuray.dadada@gmail.com>
2014-06-17 15:57:25 +0900

    Added several configure options to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 
 make clean
 ./autogen.sh
-./configure --enable-symlinks --with-haskell-flags="\
+./configure \
+    --enable-symlinks \
+    --with-os-search-path=/usr/local/share/ganeti/os \
+    --with-ssh-initscript=/etc/init.d/sshd \
+    --with-haskell-flags="\
 -hide-package=monads-tf \
 -hide-package=regex-pcre-builtin \
 "
